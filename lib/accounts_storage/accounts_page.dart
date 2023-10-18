6727d6cd92701bc7547fd3b01f1914788167b201
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-18 18:05:02 +0300

    added listview builder

--- lib/accounts_storage/accounts_page.dart
+++ lib/accounts_storage/accounts_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/accounts_storage/account_model.dart';
 
 class AccountsPage extends StatefulWidget {
   const AccountsPage({super.key});
@@ -8,10 +10,20 @@ class AccountsPage extends StatefulWidget {
 }
 
 class _AccountsPageState extends State<AccountsPage> {
+  final List<AccountModel> accounts = [
+    AccountModel(
+        username: "haydar123",
+        password: "haydar123",
+        mail: "haydar123@gmail.com",
+        mailPassword: "haydar123",
+        rank: "ascendant1",
+        dob: "01.01.1999")
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("Accounts Storage"),
         actions: [
           Padding(
@@ -23,8 +35,26 @@ class _AccountsPageState extends State<AccountsPage> {
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {}, label: const Text("Add Account")),
+      floatingActionButton: FloatingActionButton.extended(onPressed: () {}, label: const Text("Add Account")),
+      body: ListView.builder(
+        itemCount: accounts.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Image.asset("assets/png/${accounts[index].rank}.png"),
+              title: Text("${accounts[index].username}"),
+              subtitle: Text("${accounts[index].password}"),
+              trailing: IconButton(
+                  onPressed: () {
+                    Clipboard.setData(ClipboardData(
+                        text:
+                            "Username : ${accounts[index].username} | Password : ${accounts[index].password}} | Mail : ${accounts[index].mail} | Mail Password : ${accounts[index].mailPassword} | Rank : ${accounts[index].rank} | Date of Birth : ${accounts[index].dob}"));
+                  },
+                  icon: const Icon(Icons.copy)),
+            ),
+          );
+        },
+      ),
     );
   }
 }
