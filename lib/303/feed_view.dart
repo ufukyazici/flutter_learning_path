e9810142441ccc7a4f4bd0bab72b489eb2a0b819
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-31 21:32:39 +0300

    Future Builder

--- lib/303/feed_view.dart
+++ lib/303/feed_view.dart
@@ -9,34 +9,71 @@ class FeedView extends StatefulWidget {
   State<FeedView> createState() => _FeedViewState();
 }
 
-class _FeedViewState extends State<FeedView> {
+class _FeedViewState extends State<FeedView> with AutomaticKeepAliveClientMixin {
   final IPostService _postService = PostService();
+  late final Future<List<PostModel>?> _list;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _list = _postService.fetchPostItems();
+  }
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder<List<PostModel>?>(
-        future: _postService.fetchPostItems(),
-        builder: (BuildContext context, AsyncSnapshot<List<PostModel>?> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Placeholder();
-            case ConnectionState.waiting:
-              return const Center(child: CircularProgressIndicator());
-            case ConnectionState.active:
-              return const Center(child: CircularProgressIndicator());
-            case ConnectionState.done:
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+      ),
+      body: _FeedFutureBuilder(list: _list),
+    );
+  }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
+}
+
+class _FeedFutureBuilder extends StatelessWidget {
+  const _FeedFutureBuilder({
+    required Future<List<PostModel>?> list,
+  }) : _list = list;
+
+  final Future<List<PostModel>?> _list;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<PostModel>?>(
+      future: _list,
+      builder: (BuildContext context, AsyncSnapshot<List<PostModel>?> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return const Placeholder();
+          case ConnectionState.waiting:
+            return const Center(child: CircularProgressIndicator());
+          case ConnectionState.active:
+            return const Center(child: CircularProgressIndicator());
+          case ConnectionState.done:
+            if (snapshot.hasData) {
               return ListView.builder(
                 itemCount: snapshot.data?.length ?? 0,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data?[index].body ?? ""),
+                  return Card(
+                    child: ListTile(
+                      title: Text(snapshot.data?[index].body ?? ""),
+                    ),
                   );
                 },
               );
-          }
-        },
-      ),
+            } else {
+              return const Placeholder();
+            }
+        }
+      },
     );
   }
 }
