f4ed40d5bd3d0f328069dda7eefffa4b31ec5e6d
ufukyazici <ufukyazici.dev@gmail.com>
2023-11-02 21:20:20 +0300

    Provider

--- lib/303/reqres_resource/view/reqres_view.dart
+++ lib/303/reqres_resource/view/reqres_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/303/reqres_resource/viewModel/reqres_view_model.dart';
+import 'package:flutter_application_1/303/reqres_resource/model/resource_model.dart';
+import 'package:flutter_application_1/303/reqres_resource/service/reqres_service.dart';
+import 'package:flutter_application_1/303/reqres_resource/viewModel/reqres_provider.dart';
 import 'package:flutter_application_1/product/extension/string_extension.dart';
+import 'package:flutter_application_1/product/service/projectDio.dart';
+import 'package:provider/provider.dart';
 
 class ReqResView extends StatefulWidget {
   const ReqResView({super.key});
@@ -9,23 +13,40 @@ class ReqResView extends StatefulWidget {
   State<ReqResView> createState() => _ReqResViewState();
 }
 
-class _ReqResViewState extends ReqresViewModel {
+// class _ReqResViewState extends ReqresViewModel {
+class _ReqResViewState extends State<ReqResView> with ProjectDioMixin {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: resources.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            color: Color(resources[index].color?.colorValue ?? 0),
-            child: ListTile(
-              title: Text(resources[index].name ?? ""),
-              subtitle: Text(resources[index].year.toString()),
-            ),
-          );
-        },
-      ),
+    return ChangeNotifierProvider<ReqresProvider>(
+      create: (context) => ReqresProvider(ResourceService(dio: service)),
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: context.watch<ReqresProvider>().isLoading ? const CircularProgressIndicator() : null,
+          ),
+          body: _resourceListView(context.watch<ReqresProvider>().resources),
+        );
+      },
+    );
+  }
+
+  ListView _resourceListView(List<Data> items) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Color(items[index].color?.colorValue ?? 0),
+          child: ListTile(
+            title: Text(items[index].name ?? ""),
+            subtitle: Text(items[index].year.toString()),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/303/reqres_resource/viewModel/reqres_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/303/reqres_resource/model/resource_model.dart';
+import 'package:flutter_application_1/303/reqres_resource/service/reqres_service.dart';
+
+class ReqresProvider extends ChangeNotifier {
+  final IResourceService reqresService;
+  List<Data> resources = [];
+  bool isLoading = false;
+  ReqresProvider(this.reqresService) {
+    _fetch();
+  }
+  void _changeLoading() {
+    isLoading = !isLoading;
+    notifyListeners();
+  }
+
+  Future<void> _fetch() async {
+    _changeLoading();
+    resources = (await reqresService.fetchResourceItems())?.data ?? [];
+    _changeLoading();
+  }
+}
